8533ccf5903c0a6d01d7df63d7670a04894533a2
LLCo <543476459@qq.com>
2019-01-05 15:54:18 +0800

    0105

--- Model_1229.py
+++ Model_1229.py
@@ -15,10 +15,8 @@ def __loc_yield(sample_size):
         loc_1 = np.min((loc_0 + BATCH_SIZE, sample_size))
         yield loc_0, loc_1
 
-
 def __get_tensors(model_name):
     graph = tf.get_default_graph()
-
     y_place = graph.get_tensor_by_name("{}/y_palce:0".format(model_name))
     X_place = graph.get_tensor_by_name("{}/x_place:0".format(model_name))
     is_training_place = graph.get_tensor_by_name("{}/is_trainning_place:0".format(model_name))
@@ -29,7 +27,6 @@ def __get_tensors(model_name):
     ac = graph.get_tensor_by_name("{}/ac:0".format(model_name))
 
     return y_place, X_place, is_training_place, loss, predict_proba, y_predict_tf, ac
-
 
 def __cnn_layer(layer, filters, is_training, is_pool, cnn_name, bn_name):
     # regularizer = l1_l2_regularizer(scale_l1=0.1, scale_l2=0.1)
@@ -47,16 +44,17 @@ def __cnn_layer(layer, filters, is_training, is_pool, cnn_name, bn_name):
         new_layer = tf.layers.max_pooling2d(inputs=new_layer, pool_size=[2, 2], strides=2)
     return new_layer
 
-
-def __get_train_op(model_name, learning_rate=0.001):
+def __get_train_op(model_name, learning_rate=0.001, is_freeze=False):
     # Trainning Op Set
     train_op = None
     update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
     loss = tf.get_default_graph().get_tensor_by_name("{}/loss:0".format(model_name))
     with tf.control_dependencies(update_ops):
-        train_op = tf.train.AdamOptimizer(learning_rate).minimize(loss)  # var_list=training_variables
+        if is_freeze:
+            train_op = tf.train.AdamOptimizer(learning_rate).minimize(loss, var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="Map_Layers"))
+        else:
+            train_op = tf.train.AdamOptimizer(learning_rate).minimize(loss)  # var_list=training_variables
     return train_op
-
 
 def __training_one_epoch(train_set, valid_set, sess, train_op, model_name="Model", is_shuffle=False):
     '''
@@ -85,7 +83,6 @@ def __training_one_epoch(train_set, valid_set, sess, train_op, model_name="Model
     # train_ac, train_loss = batch_vali(valid_set, sess, model_name)
     return vali_ac, vali_loss
 
-
 def build_model(model_name):
     '''
     Define the names of tensor one by one, and get the parameters by name later. 
@@ -103,32 +100,38 @@ def build_model(model_name):
         # X_proba = tf.placeholder('float',[None,17], name='x_proba_palce')
         
         #OPs
-        # input BN
-        input_img = tf.layers.batch_normalization(X_place, training=is_training, name='input_BN')
+        # add AUG
+        # X_place = tf.cond(is_training, lambda : tf.image.random_flip_left_right(tf.image.random_flip_up_down(X_place)), lambda : X_place) 
+
+        with tf.variable_scope('Feature_Layers'):
+            # input BN
+            input_img = tf.layers.batch_normalization(X_place, training=is_training, name='input_BN')
+            
+            # CNN layers : 2C * MP * 2C * MP * 2C * MP
+            layer_1_0 = __cnn_layer(input_img, 32, is_training, False, cnn_name='CNN1', bn_name='CNN1_BN')
+            layer_1_1 = __cnn_layer(layer_1_0, 32, is_training, True, cnn_name='CNN2', bn_name='CNN2_BN')  # 16 * 16
+            layer_1 = tf.layers.dropout(layer_1_1, rate=0.25, training=is_training)
+            
+            layer_2_0 = __cnn_layer(layer_1, 64, is_training, False, cnn_name='CNN3', bn_name='CNN3_BN')
+            layer_2_1 = __cnn_layer(layer_2_0, 64, is_training, True, cnn_name='CNN4', bn_name='CNN4_BN')  # 8 * 8
+            layer_2 = tf.layers.dropout(layer_2_1, rate=0.25, training=is_training)
+            
+            layer_3_0 = __cnn_layer(layer_2, 128, is_training, False, cnn_name='CNN5', bn_name='CNN5_BN')
+            layer_3_1 = __cnn_layer(layer_3_0, 128, is_training, True, cnn_name='CNN6', bn_name='CNN6_BN')  # 4 * 4
+            layer_3 = tf.layers.dropout(layer_3_1, rate=0.25, training=is_training)
+            
+            # Dense layers : 1D(128) * 1D(17)
+            conv_reshape = tf.reshape(layer_3, [-1,4*4*128])
         
-        # CNN layers : 2C * MP * 2C * MP * 2C * MP
-        layer_1_0 = __cnn_layer(input_img, 32, is_training, False, cnn_name='CNN1', bn_name='CNN1_BN')
-        layer_1_1 = __cnn_layer(layer_1_0, 32, is_training, True, cnn_name='CNN2', bn_name='CNN2_BN')  # 16 * 16
-        layer_1 = tf.layers.dropout(layer_1_1, rate=0.25, training=is_training)
-        
-        layer_2_0 = __cnn_layer(layer_1, 64, is_training, False, cnn_name='CNN3', bn_name='CNN3_BN')
-        layer_2_1 = __cnn_layer(layer_2_0, 64, is_training, True, cnn_name='CNN4', bn_name='CNN4_BN')  # 8 * 8
-        layer_2 = tf.layers.dropout(layer_2_1, rate=0.25, training=is_training)
-        
-        layer_3_0 = __cnn_layer(layer_2, 128, is_training, False, cnn_name='CNN5', bn_name='CNN5_BN')
-        layer_3_1 = __cnn_layer(layer_3_0, 128, is_training, True, cnn_name='CNN6', bn_name='CNN6_BN')  # 4 * 4
-        layer_3 = tf.layers.dropout(layer_3_1, rate=0.25, training=is_training)
-        
-        # Dense layers : 1D(128) * 1D(17)
-        conv_reshape = tf.reshape(layer_3, [-1,4*4*128])
-        
-        dense = tf.layers.dense(inputs=conv_reshape, units=128, activation=None)
-        dense = tf.layers.batch_normalization(dense, training=is_training)
-        dense = tf.nn.sigmoid(dense)  # sigmoid is better but slow...
-        dense = tf.layers.dropout(dense, rate=0.25, training=is_training)
-        # dense_concat = tf.concat([dense, X_proba], 1)  # add proba in Train Set. 
-        
-        logits = tf.layers.dense(inputs=dense, units=CLASS_NUM)
+        with tf.variable_scope('Map_Layers'):
+
+            dense = tf.layers.dense(inputs=conv_reshape, units=128, activation=None)
+            dense = tf.layers.batch_normalization(dense, training=is_training)
+            dense = tf.nn.sigmoid(dense)  # sigmoid is better but slow...
+            dense = tf.layers.dropout(dense, rate=0.25, training=is_training)
+            # dense_concat = tf.concat([dense, X_proba], 1)  # add proba in Train Set. 
+            
+            logits = tf.layers.dense(inputs=dense, units=CLASS_NUM)
         
         # Output
         loss = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_place, logits=logits), name='loss')
@@ -138,12 +141,11 @@ def build_model(model_name):
         
     print('Model Build Success!')
 
-
-def training(train_set, valid_set, model_name, epochs=10, learning_rate=0.001, early_stop=None, restore_name=None, is_shuffle=False):
+def training(train_set, valid_set, model_name, epochs=10, learning_rate=0.001, early_stop=None, restore_name=None, is_shuffle=False, is_freeze=False):
 
     start_time = time.time()
     # Trainning Op Set
-    train_op = __get_train_op(model_name, learning_rate)
+    train_op = __get_train_op(model_name, learning_rate, is_freeze)
     min_loss = np.inf
     min_epoch = -1
     model_saver_path = "..\ckpt\{}.ckpt".format(model_name)
@@ -173,7 +175,6 @@ def training(train_set, valid_set, model_name, epochs=10, learning_rate=0.001, e
         if early_stop is None:
             saver.save(sess, model_saver_path)
     print("Training time using:", time.time() -start_time)
-
 	
 def batch_vali(data, sess, model_name):
     # Get Tensors
@@ -196,7 +197,6 @@ def batch_vali(data, sess, model_name):
         total_loss += batch_loss
         i += 1
     return total_ac / i, total_loss / samples
-
 
 def batch_predict(data, model_name): # get proba (sample, classnum)
     # Get Tensors
